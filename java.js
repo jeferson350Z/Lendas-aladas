00d2a6ca426eb71b623bfcb0700eb2ed92999c58
jeferson350Z <168676487+jeferson350Z@users.noreply.github.com>
2024-06-07 20:44:44 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,15 @@
+var sol = document.getElementById('sol');
+sol.addEventListener('mouseover', function () {
+    sol.style.fontSize = '30px';
+});
+sol.addEventListener('mouseout', function () {
+    sol.style.fontSize = '20px';
+});
+
+var sol = document.getElementById('sol');
+sol.addEventListener('mouseover', function () {
+    sol.style.color = 'red';
+});
+sol.addEventListener('mouseout', function () {
+    sol.style.color = 'yellow';
+});
